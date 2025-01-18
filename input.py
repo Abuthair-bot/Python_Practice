02813f04e15d473ed8323cf9842cc1132d7d6218
Abuthair <abuthairsheika11@gmail.com>
2025-01-18 18:11:34 +0530

    Input

---
+++ input.py
@@ -0,0 +1,15 @@
+# a = input()
+# print(a)
+# b = input()
+# print(b)
+# print("$"*3)
+
+# a = input()
+# b = input()
+# print("Hello "+a+b)
+
+
+a = input()
+b = input()
+
+print(a +" is "+b+" years old")
